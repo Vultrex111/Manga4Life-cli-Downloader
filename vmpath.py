14cebf5bf18bd6e7981bd8d94086d9fef8e27f79
Nini O.X <112957015+Vultrex111@users.noreply.github.com>
2024-05-29 16:41:18 +0100

    Actually extracts what i needed

--- vmpath.py
+++ vmpath.py
@@ -10,14 +10,18 @@ def extract_text_from_html(html_content):
         extracted_text = result.stdout.strip()
         lines_with_us = [line for line in extracted_text.split('\n') if 'us' in line]
         if lines_with_us:
-            mangaAddress = lines_with_us[0]
+            mangaAddress = lines_with_us[0].rstrip(';')
             print(mangaAddress)
         else:
             print("No line containing 'us' found.")
     else:
         print("No text between quotation marks found.")
 
-def extract_text_from_url(url):
+def extract_text_from_url(manga_name, manga_chapter):
+    # Replace spaces with hyphens in the manga_name
+    manga_name = manga_name.replace(" ", "-")
+    
+    url = f"https://manga4life.com/read-online/{manga_name}-chapter-{manga_chapter}.html"
     try:
         response = requests.get(url)
         if response.status_code == 200:
@@ -28,7 +32,9 @@ def extract_text_from_url(url):
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
-# Prompt the user to enter a URL
-url = input("Enter the URL: ")
-extract_text_from_url(url)
+# Prompt the user to enter the Manga Name and Manga Chapter
+manga_name = input("Enter the Manga Name: ")
+manga_chapter = input("Enter the Manga Chapter: ")
+
+extract_text_from_url(manga_name, manga_chapter)
 
